2969e0c3fcdb9c12e8d0b128a3cd11c79ffef405
Rafy Akbar <rafyakbar02@gmail.com>
2023-06-21 18:18:04 +0700

    initial layout

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-300 border-gray-200">
+      <div className="mx-auto p-4 flex items-center justify-center">
+        <p className="text-xs">All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white">
+      <div className="mx-auto p-4 flex items-center justify-between">
+        <a href="" className="font-bold">
+          diskusi
+        </a>
+        <a href="">Login</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
