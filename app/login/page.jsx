02551fd8620f72b3c388db0a3c95d0278a5ad754
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-10 18:40:29 +0700

    fixed register page and added login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import TextInput from "../components/TextInput";
 
 const page = () => {
   return (
     <section className="p-4 flex flex-col m-auto w-3/4 max-w-xl">
-      <h1 className="mb-4 mx-auto font-bold">Registration</h1>
-      <TextInput placeholder={"First Name"} />
-      <TextInput placeholder={"Last Name"} />
+      <h1 className="mb-4 mx-auto font-bold">Login</h1>
       <TextInput placeholder={"Email"} />
-      <TextInput placeholder={"Create Password"} />
-      <TextInput placeholder={"Confirm Password"} />
+      <TextInput placeholder={"Password"} />
       <a
         href="/"
         className="bg-blue-700 px-3 py-2 text-white rounded-xl mx-auto"
       >
-        Register
+        Login
       </a>
     </section>
   );
