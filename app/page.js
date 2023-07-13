e9ac27e77b8ff492e5c65321962c35e668327f4e
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-13 17:09:23 +0700

    adjust for mobile view

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Post from "./components/Post";
 
 export default function Home() {
   return (
-    <section className="p-4 flex flex-wrap flex-col gap-4 justify-center m-auto w-2/3 max-w-6xl">
+    <section className="p-4 flex flex-wrap flex-col gap-4 justify-center m-auto sm:w-2/3 max-w-3xl">
       <Post />
       <Post />
     </section>
