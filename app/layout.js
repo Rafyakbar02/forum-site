62ee50b6c7737b38fd7d26eaaf6166908811f69c
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-13 17:08:19 +0700

    changed bg color

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-300">
+      <body className="">
         <Navbar />
         {children}
         <Footer />
